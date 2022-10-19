07c1df351c2aa7b5011224766d92d6ffed7bf497
XabierOterino <xabierblockchain@gmail.com>
2022-10-19 10:29:39 +0200

    localstorage added

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-const submitButton = document.querySelector('button')
-const taskName = document.querySelector('.js-task-name')
-const taskTimes = document.querySelector('.js-task-times')
+const submitButton = document.querySelector('button');
+const taskName = document.querySelector('.js-task-name');
+const taskTimes = document.querySelector('.js-task-times');
 
-let nameInput 
-let timesInput = 1 
+let nameInput;
+let timesInput = 1; 
 
 taskName.addEventListener('change', e => {
-    e.preventDefault()
-    nameInput = e.target.value
+    e.preventDefault();
+    nameInput = e.target.value;
 })
 
 taskTimes.addEventListener('change', e => {
-    e.preventDefault()
-    timesInput = e.target.value
+    e.preventDefault();
+    timesInput = e.target.value;
 })
 
 
 submitButton.addEventListener('click' , e =>{
-    e.preventDefault()
-    updateTable(nameInput, timesInput)
+    e.preventDefault();
+    updateTable(nameInput, timesInput);
+    const items = loadState();
+    console.log(items);
+    if (!items.length){
+        saveState([{nameInput, timesInput}]);
+    }else{
+        const updated = {nameInput , timesInput};
+        saveState([...items, updated]);
+    }
+
 })
 
 const updateTable = (name, times) => {
-    const clName = `js-${name}`
-    const tableBody = document.querySelector('.js-task')
+    const clName = `js-${name}`;
+    const tableBody = document.querySelector('.js-task');
     const newRow = 
     `<tr class="row-${clName}">
         <td>${name}</td>
@@ -34,11 +43,12 @@ const updateTable = (name, times) => {
             <button class="delete-${clName}" >Delete task</button>
         </td>
     </tr>
-    `
+    `;
     tableBody.innerHTML += newRow
     document.querySelector(`.done-${clName}`).addEventListener('click', e => {
         e.preventDefault()
         document.querySelector(`.${clName}`).innerHTML = '<p>Finished</p>'
+        saveState(loadState().filter(i => i.nameInput!=name));
     })
 
     document.querySelector(`.increase-${clName}`).addEventListener('click', e => {
@@ -47,14 +57,30 @@ const updateTable = (name, times) => {
         document.querySelector(`.span-${clName}`).innerText = 
             parseInt(document.querySelector(`.span-${clName}`).innerText) < total 
             ? parseInt(document.querySelector(`.span-${clName}`).innerText) + 1
-            : parseInt(document.querySelector(`.span-${clName}`).innerText)
+            : parseInt(document.querySelector(`.span-${clName}`).innerText);
+        
+        let updated = loadState()
+        console.log(updated[updated.indexOf(updated.find(i=> i.nameInput == name))].timesInput )
+        // saveState(updated);
     })
 
     document.querySelector(`.delete-${clName}`).addEventListener('click' , e => {
         e.preventDefault()
         document.querySelector(`.row-${clName}`).parentNode.removeChild(document.querySelector(`.row-${clName}`))
+        saveState(loadState().filter(i => i.nameInput!=name));
     })
 
     
 }
 
+
+
+const saveState = tasks => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+const loadState = () => {
+    return JSON.parse(localStorage.getItem('tasks')) || [];
+}
+
+loadState()?.map(i => updateTable(i.nameInput, parseInt(i.timesInput)))
